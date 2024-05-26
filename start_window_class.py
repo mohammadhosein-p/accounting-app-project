f16723beddd9b98a82f16037cc1c3023c655ea64
mohammadhosein-p <150898771+mohammadhosein-p@users.noreply.github.com>
2024-05-26 22:23:20 +0430

    added start_widnow_class

---
+++ start_window_class.py
@@ -0,0 +1,70 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QDialog, QLabel, QPushButton, QHBoxLayout
+from PyQt5.QtGui import QFont
+import sys
+
+app = QApplication(sys.argv)
+
+class StartWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setFixedSize(400, 300)
+        self.setWindowTitle("Start Window")
+        self.setStyleSheet("background-color: #003C43")
+        self.label = QLabel("Welcome to the app!", self)
+        self.label.setGeometry(80, 50, 280, 50)
+        self.label.setFont(QFont("Times New Roman", 18))
+        self.label.setStyleSheet("color: #EEEEEE;")
+        self.hbox = QHBoxLayout(self)
+
+        self.login_btn = QPushButton("Login", self)
+        self.login_btn.setFixedSize(100, 35)
+        self.login_btn.setFont(QFont("Times New Roman", 14))
+        self.login_btn.setStyleSheet("""
+            QPushButton {
+                border: None;
+                background-color: #76ABAE; 
+                border-radius: 8px; 
+                color: #EEEEEE;
+                font-size: 18px;
+                font-weight: bold;
+            }
+            QPushButton:hover {
+                background-color: #EEEEEE;
+                color: #003C43;
+            }
+        """)
+        self.sign_up_btn = QPushButton("Sign Up", self)
+        self.sign_up_btn.setFixedSize(100, 35)
+        self.sign_up_btn.setFont(QFont("Times New Roman", 14))
+        self.sign_up_btn.setStyleSheet("""
+            QPushButton {
+                border: None;
+                background-color: #76ABAE; 
+                border-radius: 8px; 
+                color: #EEEEEE;
+                font-size: 18px;
+                font-weight: bold;
+            }
+            QPushButton:hover {
+                background-color: #EEEEEE;
+                color: #003C43;
+            }
+        """)
+        self.hbox.addWidget(self.login_btn)
+        self.hbox.addWidget(self.sign_up_btn)
+        self.sign_up_btn.clicked.connect(self.create_sign_up_page)
+        self.login_btn.clicked.connect(self.create_login_page)
+
+    def create_sign_up_page(self):
+        print("sign up")
+        
+    def create_login_page(self):
+        print("log in")
+
+
+        
+
+window = StartWindow()
+window.show()
+sys.exit(app.exec())
